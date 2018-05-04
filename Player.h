f5cce73061095d793e5f22f69e65db7f516d50d8
Mário Gil <mg_mesquita@hotmail.com>
2018-05-04 18:12:59 +0100

    Player class

---
+++ Player.h
@@ -0,0 +1,25 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>;
+
+class Player
+{
+public:
+	// Constructors
+	Player();
+	Player(std::string name);
+
+	// Getters
+	std::string getName();
+	double getTotalTime();
+	int getNumberOfClues();
+
+private:
+	std::string name;
+	double initialTime, finalTime, totalTime;
+	int numberOfClues = 0;
+};
+
+
+#endif
